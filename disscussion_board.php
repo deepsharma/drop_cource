7b673cc2ebef723b525f1dabae15b6605cb1e28b
qustn_deepaks <deepak@qustn.com>
2017-05-20 16:20:34 +0530

    discussion_board

---
+++ disscussion_board.php
@@ -0,0 +1,262 @@
+<?php
+require 'include/header.php';
+?>
+
+
+		<div class="bg-color-sunsetOrange fh5co-cover fh5co-cover-style-2" data-stellar-background-ratio="0.5" data-next="yes">
+		 
+			<div class="fh5co-cover-text">
+				<div class="container">
+					<div class="row">
+						<section class="col-md-push-0 col-md-12">
+						<div class="fh5co-cover-intro">
+							<ul class="tab__content ml-1p">
+								<li class="">
+									<div class="content__wrapper">
+									<div class="row">
+										<div class="col-md-7">
+										<h4 class="text-left fs-20">Over 500 digital courses across these topics</h4>
+										<div class="row" style="padding: 10px">
+											<div class="col-md-5">
+												<div class="media">
+												  <div class="media-left">
+												  <i class="fa fa-money icon-tab-list" aria-hidden="true"></i>
+												  </div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Sales</p>
+													<p class="topic-description hidden-xs">Inside sales or direct sales.<br>World class content</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-bullhorn icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Marketing </p>
+													<p class="topic-description hidden-xs">Make your team great at<br>digital or offline marketing</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-child icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Personality Development</p>
+													<p class="topic-description hidden-xs">Ensure that your team <br>becomes the best they can be </p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-users icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Human Resources</p>
+													<p class="topic-description hidden-xs">Make your HR team<br>a powerhouse</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-code icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Technology</p>
+													<p class="topic-description hidden-xs">Probably the best content to <br>grow your Coder into Ninjas</p>
+												  </div>
+												</div>
+											</div>
+											<div class="col-md-5">
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-comments-o icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Communication</p>
+													<p class="topic-description hidden-xs">Presentation to Pitches.<br>We have the best trainings</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-sitemap icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Leadership</p>
+													<p class="topic-description hidden-xs">Leaders can be made.<br>Our trainings show you how</p>
+												  </div> 
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-usd icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Finance</p>
+													<p class="topic-description hidden-xs">Finance 101 or business planing.<br>We have all the trainings</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-phone icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Customer Service</p>
+													<p class="topic-description hidden-xs">Make your customers love you.<br>Train your agents for all situations</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-check-square-o icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Compliance</p>
+													<p class="topic-description hidden-xs">Sexual harrasment to Ethics.<br>Do what is right</p>
+												  </div>
+												</div>
+											</div>
+											<div class="col-md-2"></div>
+											</div>
+										</div>
+										<div class="hidden-md">&nbsp;</div>
+										<div class="col-md-4 text-center">
+										<h4 class="fs-20 mb-10">Available just the way you want it</h4>
+											<p class="fs-20 mb-0">Choose from </p>
+											<img src="images/content-ingographic.png" alt="Individual Courses and Bundled Packs" class="img-responsive mb-0" style="margin-left: auto; margin-right: auto">
+											<p class="fs-20 mt-0">Delivered on our award winning Mobile first platform</p>
+											<a href="#" class="btn btn-success btn-blue"  data-toggle="modal" data-target="#interested_in_content">I am interested in content</a>
+										</div>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="content__wrapper">
+										<div class="row">
+										<div class="col-md-7">
+										<h4 class="text-left fs-20">A Mobile first platform that solves your needs</h4>
+										<div class="row" style="padding: 10px">
+											<div class="col-md-5">
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-folder-open icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Any Content</p>
+													<p class="topic-description hidden-xs">Use SCORM, Video, PPT <br>or any other format</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-mobile icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Any Device </p>
+													<p class="topic-description hidden-xs">IOS, Android or Web.<br>We work across all</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-pencil icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Simple Authoring</p>
+													<p class="topic-description hidden-xs">One of the simplest authoring <br>tool for any course</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-thumbs-up icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Tried and Tested</p>
+													<p class="topic-description hidden-xs">Deliverd over 500,000 trainings to over 70,000 people</p>
+												  </div>
+												</div>
+											</div>
+											<div class="col-md-5">
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-smile-o icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Simple to Use</p>
+													<p class="topic-description hidden-xs">Intuitive web based admin panel and simple application</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-sliders icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">DIY Platform</p>
+													<p class="topic-description hidden-xs">You will never need to run <br>to IT again</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-link icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Integration APIs Available</p>
+													<p class="topic-description hidden-xs">Integrate with existing systems or your app</p>
+												  </div>
+												</div>
+												<div class="media">
+												  <div class="media-left"><i class="fa fa-line-chart icon-tab-list" aria-hidden="true"></i></div>
+												  <div class="media-body text-left" style="line-height: 25px">
+													<p class="mb-0">Great Analytics</p>
+													<p class="topic-description hidden-xs">Standard or detailed reports available</p>
+												  </div>
+												</div>
+											</div>
+											<div class="col-md-2"></div>
+										</div>
+										<div class="hidden-md">&nbsp;</div>
+										<div class="row" style="margin-left: 12px; margin-top: 10px">
+												<h4 class="text-left fs-20 mb-10">Tested and Awarded by</h4>	
+												<img src="images/awards.png" alt="Awards" class="img-responsive mb-0" style="margin-left: 0px; width:75%">
+										</div>
+										</div>
+										<div class="col-md-4 text-center pb-12 pt-0">
+										<h4 class="fs-20 mb-50">Use it in the manner you want</h4>
+											<img src="images/lms-ingographic.png" alt="Individual Courses and Bundled Packs" class="img-responsive mb-50" style="margin-left: auto; margin-right: auto">
+											<a href="#" class="btn btn-success btn-blue"  data-toggle="modal" data-target="#i_want_a_demo">I want a demo</a>
+										</div>
+										</div>
+									</div>
+								</li>
+							</ul>
+							</div>
+						</section>
+
+						
+						
+						
+					</div>
+				</div>	
+			</div>
+		</div>
+
+
+		<div class="fh5co-project-style-2 discussion_container" id="partner-section">
+			<div class="row p-b " style="margin:0px" >
+				<div class="col-xs-12 col-md-12" >
+					<h1 class="discussion_head">Discussion Board<h1>
+					<p class="discussion_text">This is an area for our community to talk to each other and<br> collaborate opportunities. </p>
+				</div>
+			</div>
+			</div>
+		</div>		
+		
+
+		<div class="fh5co-blog-style-1 discussion-text-dialog">
+			<div class="container" >
+				<div class="row" >
+
+					<div class="col-md-12 text-center discussion-text-dialog-main-container">
+					<div class="discussion-text-dialog-inner-container">
+						<div class="post">
+							<img class="discussion-img"  src="images/80049701.jpg">
+						<div class="discussion-post-title">Review suppliers</div>
+							<div class="discussion-post-desc">
+							<p>12/12/2016</p>
+							<p>This space is for suppliers and manufacturers feedback...</p>
+							</div>
+						</div>
+						
+						<div class="post">
+							<img class="discussion-img" src="images/164123455_1000.jpg">
+						<div class="discussion-post-title">Review suppliers</div>
+							<div class="discussion-post-desc">
+							<p>12/12/2016</p>
+							<p>This space is for suppliers and manufacturers feedback...</p>
+							</div>
+						</div>
+						
+						
+					</div>
+					</div>
+		</div>
+		
+			<div class="row" >
+
+						<div class="col-md-12 text-center discussion-text-dialog-main-container">
+						<div class="discussion-text-dialog-inner-container">
+							<div class="post">
+								TAGS
+							</div>
+							
+							
+						</div>
+						</div>
+			</div>
+		</div>
+		</div>
+		
+							
+								
+<?php include 'include/footer.php'; ?>
